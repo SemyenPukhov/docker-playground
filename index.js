34a0b37ce997764a26a1f4f03bd41c59fd3ac1e5
Semyen Pukhov <sspukhov@yandex.ru>
2019-08-06 16:41:23 +0300

    Recommit latest changes

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+const port = process.env.PORT || 5000;
+
+const mongoDB = 'mongodb://mongo:27017/docker-test';
+
+
+mongoose.connect(mongoDB, { useNewUrlParser: true }, err => {
+  console.log('err', err);
+  if (!err) console.log('Db is ready');
+});
+
+app.use(express.json());
+app.use(cors());
+
+app.use('/', (req, res, next) => {
+  // For cherry pick from other remote
+  next();
+})
+
+app.use('/user', require('./routes/userRoutes'));
+
+app.listen(port, () =>
+  console.log(`Docker-test app listening on port ${port}!`)
+);
